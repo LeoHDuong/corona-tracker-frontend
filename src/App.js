19ec3c5f0cf9af92e66010a0ac9f35be4dc30f58
Leo Duong <lanhong.duong101005@gmail.com>
2026-06-10 14:50:33 +0700

    modify api to use k8s domain

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
 
 
   componentWillMount() {
-    axios.get('http://localhost:8080/coronatracker/statisticsbycountry').then((response) => {
+    axios.get('http://kubernetes-master/coronatracker/statisticsbycountry').then((response) => {
       //console.log(JSON.stringify(response.data))
       this.setState({
         countryWiseData: response.data.data
